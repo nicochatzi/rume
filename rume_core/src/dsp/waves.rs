7f9d484be1d3ca8313559278dc18338a7690ed4c
Nico Chatzi <nico.chatzigianis@focusrite.com>
2020-12-14 23:21:37 +0100

    Add tests for basic waveforms

--- rume_core/src/dsp/waves.rs
+++ rume_core/src/dsp/waves.rs
@@ -1,11 +1,11 @@
 pub mod saw {
     #[inline(always)]
-    pub fn rising(phase: f32) -> f32 {
+    pub fn rise(phase: f32) -> f32 {
         phase * 2. - 1.
     }
 
     #[inline(always)]
-    pub fn falling(phase: f32) -> f32 {
+    pub fn fall(phase: f32) -> f32 {
         1. - phase * 2.
     }
 }
@@ -20,5 +20,62 @@ pub fn pwm(phase: f32, duty: f32) -> f32 {
     match phase < duty {
         true => 1.0,
         false => -1.0,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn rising_saw() {
+        assert_eq!(saw::rise(0.0), -1.);
+        assert_eq!(saw::rise(0.25), -0.5);
+        assert_eq!(saw::rise(0.5), 0.);
+        assert_eq!(saw::rise(0.75), 0.5);
+        assert_eq!(saw::rise(1.0), 1.);
+    }
+
+    #[test]
+    fn falling_saw() {
+        assert_eq!(saw::fall(0.0), 1.);
+        assert_eq!(saw::fall(0.25), 0.5);
+        assert_eq!(saw::fall(0.5), 0.);
+        assert_eq!(saw::fall(0.75), -0.5);
+        assert_eq!(saw::fall(1.0), -1.);
+    }
+
+    #[test]
+    fn basic_square() {
+        assert_eq!(square(0.0), 1.0);
+        assert_eq!(square(0.1), 1.0);
+        assert_eq!(square(0.2), 1.0);
+        assert_eq!(square(0.3), 1.0);
+        assert_eq!(square(0.4), 1.0);
+        assert_eq!(square(0.499), 1.0);
+        assert_eq!(square(0.5), -1.0);
+        assert_eq!(square(0.6), -1.0);
+        assert_eq!(square(0.7), -1.0);
+        assert_eq!(square(0.8), -1.0);
+        assert_eq!(square(0.9), -1.0);
+        assert_eq!(square(1.0), -1.0);
+    }
+
+    #[test]
+    fn pulse_width() {
+        const N: usize = 100;
+        for x in 0..N {
+            let x = x as f32 / N as f32;
+            assert_eq!(pwm(x, 0.5), square(x));
+
+            for d in 0..N {
+                let d = d as f32 / N as f32;
+                if x < d {
+                    assert_eq!(pwm(x, d), 1.0);
+                } else {
+                    assert_eq!(pwm(x, d), -1.0);
+                }
+            }
+        }
     }
 }
