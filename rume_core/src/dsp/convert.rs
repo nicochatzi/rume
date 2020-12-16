ee610f947bf55673abfc26cd07f93cdc47831f5f
Nico Chatzi <nico.chatzigianis@focusrite.com>
2020-12-16 19:18:50 +0100

    Add tests for convert

--- rume_core/src/dsp/convert.rs
+++ rume_core/src/dsp/convert.rs
@@ -8,6 +8,7 @@ pub mod tick {
     /// Convert from a number of ticks to a number of milliseconds.
     #[inline(always)]
     pub fn to_millis(ticks: f32, rate: f32) -> f32 {
+        assert!(rate > 0.);
         ticks * 1000. / rate
     }
 
@@ -20,24 +21,28 @@ pub mod tick {
     /// Convert from a number of ticks to a number of beats-per-minute.
     #[inline(always)]
     pub fn to_bpm(ticks: f32, rate: f32) -> f32 {
+        assert!(ticks > 0.);
         rate * 60. / ticks
     }
 
     /// Convert from a number of beats-per-minute to a number of ticks.
     #[inline(always)]
     pub fn from_bpm(bpm: f32, rate: f32) -> f32 {
+        assert!(bpm > 0.);
         rate * 60. / bpm
     }
 
     /// Convert from a number of ticks to a pitch in hertz.
     #[inline(always)]
     pub fn to_pitch(ticks: f32, rate: f32) -> f32 {
+        assert!(ticks > 0.);
         rate / ticks
     }
 
     /// Convert from a pitch in hertz to a number of ticks.
     #[inline(always)]
     pub fn from_pitch(pitch: f32, rate: f32) -> f32 {
+        assert!(pitch > 0.);
         rate / pitch
     }
 }
@@ -48,10 +53,9 @@ pub mod pitch {
     use crate::F32Extension;
 
     /// From a frequency in hertz to a number of cycles per sample.
-    /// Takes a frequency and a sampling time.
     #[inline(always)]
-    pub fn to_cycles(freq: f32, time: f32) -> f32 {
-        freq * time
+    pub fn to_cycles(freq: f32, rate: f32) -> f32 {
+        freq / rate
     }
 
     ///
@@ -63,12 +67,14 @@ pub mod pitch {
     ///
     #[inline(always)]
     pub fn to_midi(freq: f32) -> f32 {
+        assert!(freq >= 0.);
         12. * (freq / 440.).log2() + 69.
     }
 
     ///
     #[inline(always)]
     pub fn from_midi(midi: f32) -> f32 {
+        assert!(midi >= 0.);
         440. * 2_f32.powf((midi - 69.) / 12.)
     }
 }
@@ -95,8 +101,6 @@ pub mod db {
     #[cfg(not(feature = "std"))]
     use crate::F32Extension;
 
-    const LOG_TEN: f32 = 2.302585092994046;
-
     ///
     #[inline(always)]
     pub fn to_gain(db: f32) -> f32 {
@@ -114,52 +118,6 @@ pub mod db {
         }
         20. * gain.log10()
     }
-
-    ///
-    #[inline(always)]
-    pub fn to_rms(db: f32) -> f32 {
-        to_power_unscaled(db, 0.05, 485.)
-    }
-
-    ///
-    #[inline(always)]
-    pub fn from_rms(rms: f32) -> f32 {
-        from_power_unscaled(rms, 20.)
-    }
-
-    ///
-    #[inline(always)]
-    pub fn to_pow(db: f32) -> f32 {
-        to_power_unscaled(db, 0.1, 870.)
-    }
-
-    ///
-    #[inline(always)]
-    pub fn from_pow(power: f32) -> f32 {
-        from_power_unscaled(power, 10.)
-    }
-
-    #[inline(always)]
-    fn to_power_unscaled(db: f32, scale: f32, max: f32) -> f32 {
-        if db <= 0. {
-            return 0.;
-        }
-        if db > max {
-            return max;
-        }
-        (LOG_TEN * scale).exp() * (db - 100.)
-    }
-
-    #[inline(always)]
-    fn from_power_unscaled(power: f32, scale: f32) -> f32 {
-        if power > 0. {
-            let val = scale / LOG_TEN * power.log10();
-            if val >= 0. {
-                return val;
-            }
-        }
-        0.
-    }
 }
 
 #[cfg(test)]
@@ -168,19 +126,73 @@ mod test {
     use core::f32::consts::PI;
 
     #[test]
-    fn pitch_to_cycles_per_second() {}
-
-    #[test]
-    fn pitch_from_cycles_per_second() {}
-
-    #[test]
-    fn pitch_to_midi() {
-        // println!("{}", pitch::to_midi(440.));
-        // println!("{}", pitch::from_midi(70.));
+    fn tick_to_millis() {
+        assert_eq!(tick::to_millis(48_000., 48_000.), 1000.);
+        assert_eq!(tick::to_millis(24_000., 48_000.), 500.);
+        assert_eq!(tick::to_millis(1_000., 100_000.), 10.);
+        assert_eq!(tick::to_millis(48., 48_000.), 1.);
     }
 
     #[test]
-    fn pitch_from_midi() {}
+    fn tick_from_millis() {
+        assert_eq!(tick::from_millis(1., 48_000.), 48.);
+        assert_eq!(tick::from_millis(500., 48_000.), 24_000.);
+    }
+
+    #[test]
+    fn tick_to_bpm() {
+        assert_eq!(tick::to_bpm(1., 1.), 60.);
+        assert_eq!(tick::to_bpm(24_000., 48_000.), 120.);
+        assert_eq!(tick::to_bpm(10_000., 48_000.), 288.);
+    }
+
+    #[test]
+    fn tick_from_bpm() {
+        assert_eq!(tick::from_bpm(1., 1.), 60.);
+        assert_eq!(tick::from_bpm(60., 48_000.), 48_000.);
+        assert_eq!(tick::from_bpm(120., 48_000.), 24_000.);
+    }
+
+    #[test]
+    fn tick_to_pitch() {
+        assert_eq!(tick::to_pitch(1., 48_000.), 48_000.);
+        assert_eq!(tick::to_pitch(24_000., 48_000.), 2.);
+        assert_eq!(tick::to_pitch(1_500., 48_000.), 32.);
+    }
+
+    #[test]
+    fn tick_from_pitch() {
+        assert_eq!(tick::from_pitch(1., 1.), 1.);
+        assert_eq!(tick::from_pitch(1., 2.), 2.);
+        assert_eq!(tick::from_pitch(32., 48_000.), 1_500.);
+    }
+
+    #[test]
+    fn pitch_to_cycles_per_second() {
+        assert_eq!(pitch::to_cycles(1., 1.), 1.);
+        assert_eq!(pitch::to_cycles(1., 4.), 0.25);
+        assert_eq!(pitch::to_cycles(48., 48_000.), 0.001);
+    }
+
+    #[test]
+    fn pitch_from_cycles_per_second() {
+        assert_eq!(pitch::from_cycles(1., 1.), 1.);
+        assert_eq!(pitch::from_cycles(0.01, 48.), 0.48);
+    }
+
+    #[test]
+    fn pitch_to_midi() {
+        assert_eq!(pitch::to_midi(8372.018), 120.);
+        assert_eq!(pitch::to_midi(440.), 69.);
+        assert_eq!(pitch::to_midi(46.249302), 30.);
+    }
+
+    #[test]
+    fn pitch_from_midi() {
+        assert_eq!(pitch::from_midi(120.), 8372.018);
+        assert_eq!(pitch::from_midi(69.), 440.);
+        assert_eq!(pitch::from_midi(30.), 46.249302);
+    }
 
     #[test]
     fn radians_to_degrees() {
@@ -200,5 +212,27 @@ mod test {
         assert_eq!(rad::from_deg(180.), PI);
         assert_eq!(rad::from_deg(270.), 3. * PI / 2.);
         assert_eq!(rad::from_deg(360.), 2. * PI);
+    }
+
+    fn assert_near(left: f32, right: f32) {
+        const EPSILON: f32 = 0.03;
+        assert!(left.abs() < (right.abs() + EPSILON));
+        assert!(left.abs() > (right.abs() - EPSILON));
+    }
+
+    #[test]
+    fn db_to_gain() {
+        assert_near(db::to_gain(6.), 2.);
+        assert_near(db::to_gain(3.), 1.41);
+        assert_near(db::to_gain(0.), 1.);
+        assert_near(db::to_gain(-3.), 0.71);
+        assert_near(db::to_gain(-6.), 0.5);
+    }
+
+    #[test]
+    fn db_from_gain() {
+        assert_near(db::from_gain(2.), 6.);
+        assert_near(db::from_gain(1.), 0.);
+        assert_near(db::from_gain(0.5), -6.);
     }
 }
